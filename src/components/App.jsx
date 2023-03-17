dd4433d3491fcc9367a6a4a6c411c272157f81b9
Kindual <110603179+Kindual@users.noreply.github.com>
2023-03-17 17:12:22 +0200

    complit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,18 @@ import ContactForm from './ContactForm/ContactForm'
 import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
 import Section from './Section/Section'
+import contacts from '../contacts.json'
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: localStorage.getItem('contacts') ? JSON.parse(localStorage.getItem('contacts')) : contacts,
     filter: '',
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
   }
 
   addContact = (obj) => {
